d20388b1debf90507c5b9e9dee61b18d1da88420
Danieldd96 <danieldelgado.dd270@gmail.com>
2025-01-26 13:19:46 -0600

    Cambios en los estilos de home agregado de espacios y estilos a los iconos del navPc

--- front/src/Components/NavPC/NavPC.jsx
+++ front/src/Components/NavPC/NavPC.jsx
@@ -46,11 +46,11 @@ const NavPC = () => {
   return (
       <>
         <nav className='NavPC'>
-          <div onClick={() => Navigation('/home')}><HomeIcon style={{fontSize: 36}}/></div>
-          <div onClick={() => Navigation('/asistencia')}><AppRegistrationIcon style={{fontSize: 36}}/></div>
-          {['lider', 'director'].includes(RoleIdData) ? <div onClick={() => Navigation('/justificaciones')}><DriveFileRenameOutlineIcon style={{fontSize: 36}}/></div> : null}
-          {RoleIdData == 'colaborador' ? <div onClick={() => Navigation('/justificacion')}><DriveFileRenameOutlineIcon style={{fontSize: 36}}/></div> : null}
-          {RoleIdData == 'director' ? <div onClick={() => Navigation('/crud')}><FolderSharedIcon style={{fontSize: 36}}/></div> : null}
+          <div onClick={() => Navigation('/home')}><HomeIcon style={{fontSize: 36}} className='icons'/></div>
+          <div onClick={() => Navigation('/asistencia')}><AppRegistrationIcon style={{fontSize: 36}} className='icons'/></div>
+          {['lider', 'director'].includes(RoleIdData) ? <div onClick={() => Navigation('/justificaciones')}><DriveFileRenameOutlineIcon style={{fontSize: 36}} className='icons'/></div> : null}
+          {RoleIdData == 'colaborador' ? <div onClick={() => Navigation('/justificacion')}><DriveFileRenameOutlineIcon style={{fontSize: 36}} className='icons'/></div> : null}
+          {RoleIdData == 'director' ? <div onClick={() => Navigation('/crud')}><FolderSharedIcon style={{fontSize: 36}} className='icons'/></div> : null}
         </nav>
       </>
   )
