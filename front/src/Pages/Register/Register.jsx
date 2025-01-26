ede656b4329bd0b973dd1a6a2ebcc69e37743ddd
Danieldd96 <danieldelgado.dd270@gmail.com>
2025-01-25 20:01:41 -0600

    Recuperacion del login con su css y creacion de estilos para el register

--- front/src/Pages/Register/Register.jsx
+++ front/src/Pages/Register/Register.jsx
@@ -3,7 +3,6 @@ import FormularioRegister from "../../Components/PruebaRegister/PruebaRegister"
 const Register = () => {
   return (
     <div>
-        <h1>Register</h1>
         <FormularioRegister/>
     </div>
   )
